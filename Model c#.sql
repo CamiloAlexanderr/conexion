6ef723157e79d269ae8322e0854311503b94ce1a
diego <diegovillalobos2@hotmail.com>
2019-09-30 15:43:15 -0500

    Se creo la base de datos en Workbench-MySQL y se implementó en los archivos

---
+++ Model c#.sql
@@ -0,0 +1,26 @@
+create database Prueba;
+
+use prueba;
+
+create table datos(
+id_wey INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+Nombre varchar(40),
+Apellido varchar(40)
+
+);
+ select*from datos;
+ 
+ insert into datos values(1,"Benito","Camelo");
+  insert into datos values(2,"Elma","Canon");
+   insert into datos values(3,"Aniseto","Delhoyoaguado");
+   insert into datos values(4,"Aquiles","Baeza");
+   insert into datos values(5,"Diego","Baeza");
+   insert into datos values(6,"Yuca","Six");
+   insert into datos values(7,"Dexter","Rodriguez");
+   insert into datos values(8,"Maximo","Sanchez");
+   insert into datos values(9,"Termometro","Sanchez");
+   insert into datos values(10,"Bolillo","Tirado");
+   insert into datos values(11,"Alejandra","Gonzales");
+   
+   
+ 
